97e38dabe4ed6abde074ea990de5be5876045cbc
Dijosto <32780744+Dijosto@users.noreply.github.com>
2017-10-16 15:09:55 -0600

    Update HANGMANNNNNN.py

--- HANGMANNNNNN.py
+++ HANGMANNNNNN.py
@@ -64,3 +64,33 @@ def displayBoard(hangmanascii, missedLetters, correctLetters, secretWord):
         if secretWord[i] in correctLetters:
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
             
+    for letter in blanks: # puts the correct words in
+        print(letter, end=' ')
+    print()
+def getGuess(guessed):
+    #returns guessed letters
+    while True:
+        print('Guess a letter.')
+        guess = input() #input for users letter
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Only single letters.')
+        elif guess in guessed:
+            print('You guessed this letter already, try again!')
+        elif guess not in alphabet:
+            print('You must enter a letter')
+        else:
+            return guess
+def playAgain():
+    #if the player wants to play again
+    print('Do you want to play again? (y/n)')
+    return input().lower().startswith('y')
+
+print('H A N G M A N')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+white True:
+    displayBoard(hangmanascii, missedLetters, correctLetters, secretWord)
